0a4bd6abcbfd6ee37bdf2d065ec696bab8010be7
Mani S <mani.s@isteer.com>
2026-05-12 16:41:22 +0530

    Delete EmployeeManagement directory

--- EmployeeManagement/src/main/java/com/company/employeeManagement/repo/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.company.employeeManagement.repo;
-
-import com.company.employeeManagement.model.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository
-        extends JpaRepository<Team, Long> {
-}
--- EmployeeManagement/src/main/java/com/company/employeeManagement/util/AppConstants.java
+++
@@ -1,7 +0,0 @@
-package com.company.employeeManagement.util;
-
-public class AppConstants {
-
-    public static final String SUCCESS =
-            "SUCCESS";
-}
